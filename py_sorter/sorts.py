64efdef87be76af5a8d7224b2611eac271c9f068
underhood31 <manavjeet18295@iiitd.ac.in>
2019-10-01 22:06:41 +0530

    Added Shell sort

--- py_sorter/sorts.py
+++ py_sorter/sorts.py
@@ -291,3 +291,32 @@ def max_heapify(arr, i, n):
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
         max_heapify(arr, largest, n)
+
+def shell_sort(integers):
+    """The idea of shellSort is to allow exchange of far items. 
+    An h-sorted array is made for a large value of h. 
+    We keep reducing the value of h until it becomes 1. """
+    integers_clone = list(integers)
+    
+    #length of the list
+    list_len = len(integers_clone)
+
+    # Select a big initial value of gap
+    jumps=list_len//2 
+
+    while jumps>=1:
+        for i in range(jumps,list_len):
+            
+            temp = integers_clone[i]
+            j=i
+
+            while j>=jumps and integers_clone[j-jumps]>temp:
+                integers_clone[j] = integers_clone[j-jumps]
+                j-=jumps
+            integers_clone[j]=temp
+        jumps//=2
+    
+    return integers_clone
+
+             
+    
